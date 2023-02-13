f20ee4ea4afab8f3a65dc0887c4531bffbae6435
Nikita Lukinov <nikita.lukinov.wrk@gmail.com>
2023-02-13 18:22:05 +0100

    made fourth homework, tested

---
+++ hw4.py
@@ -0,0 +1,42 @@
+"""
+Дано целое, положительное, ТРЁХЗНАЧНОЕ число. Например: 123, 867, 374. Необходимо его перевернуть.
+Например, если ввели число 123, то должно получиться на выходе ЧИСЛО 321. ВАЖНО! Работать только с
+числами. Строки, оператор IF и циклы использовать НЕЛЬЗЯ!
+
+На выходе ОБЯЗАТЕЛЬНО должно быть ЧИСЛО!!!
+"""
+
+
+def main():
+    # take user input
+    while True:
+        number = input("3 digit Number: ")
+        try:
+            number = int(number)
+
+            if number < 100 or number > 999:
+                print("Number must be 3 digit")
+                continue
+            break
+        except ValueError:
+            print("Number must be an integer")
+
+    # reverse without for loop (stupid)
+    digit = number % 10
+    new_number = digit * 100
+
+    number = number // 10
+
+    digit = number % 10
+    new_number += digit * 10
+
+    number = number // 10
+
+    digit = number % 10
+    new_number += digit * 1
+
+    print(new_number)
+
+
+if __name__ == "__main__":
+    main()
