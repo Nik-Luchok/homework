ed6310ac53f88854b524afdfbf07f94770b2bfa9
Nikita Lukinov <nikita.lukinov.wrk@gmail.com>
2023-02-14 17:35:27 +0100

    made hw5, tested

--- hw5.py
+++ hw5.py
@@ -7,3 +7,121 @@
 Понадобится оператор if, and, or, возможно пригодится функция abs(),
 но не обязательно.
 """
+# declare 2 dicts and a list
+pos_current_dict = {}
+pos_desired_dict = {}
+sucess_list = []
+
+
+# take input from user - 1.chess figure position and 2. where he wants to go
+# DATA example: g5 or G5. others aren't acceptable
+while True:
+    pos_current = input("Current Knight's position (A6, B3..): ")
+
+    # validate data
+    if len(pos_current) != 2:
+        print("Input must be: A6, B8, H3...")
+        continue
+
+    if not pos_current[0].isalpha():
+        print("Input must be: A6, B8, H3...")
+        continue
+    if not pos_current[1].isnumeric():
+        print("Input must be: A6, B8, H3...")
+        continue
+
+    # convert data for later and validate whether values are in range 1-8 A-H
+    pos_current = pos_current.upper()
+    pos_current_dict["char"] = ord(pos_current[0])
+    if pos_current_dict["char"] > ord("H"):
+        print("Letter must be from A to H")
+        continue
+
+    pos_current_dict["num"] = int(pos_current[1])
+    if pos_current_dict["num"] < 1 or pos_current_dict["num"] > 8:
+        print("Number must be from 1 to 8")
+        continue
+
+    # if all good break the loop
+    break
+
+while True:
+    pos_desired = input("Desired Knight's position (A6, B3..): ")
+
+    # validate data
+    if len(pos_desired) != 2:
+        print("Input must be: A6, B8, H3...")
+        continue
+
+    if not pos_desired[0].isalpha():
+        print("Input must be: A6, B8, H3...")
+        continue
+    if not pos_desired[1].isnumeric():
+        print("Input must be: A6, B8, H3...")
+        continue
+
+    # convert data for later and validate whether values are in range 1-8 A-H
+    pos_desired = pos_desired.upper()
+    pos_desired_dict["char"] = ord(pos_desired[0])
+    if pos_desired_dict["char"] > ord("H"):
+        print("Letter must be from A to H")
+        continue
+
+    pos_desired_dict["num"] = int(pos_desired[1])
+    if pos_desired_dict["num"] < 1 or pos_desired_dict["num"] > 8:
+        print("Number must be from 1 to 8")
+        continue
+
+    # if all good break the loop
+    break
+
+
+# check if desired position attainable
+if (pos_desired_dict["char"] == pos_current_dict["char"] or
+        pos_desired_dict["num"] == pos_current_dict["num"]):
+    answer = False
+    print(answer)
+    exit()
+
+if pos_desired_dict["char"] > pos_current_dict["char"]:
+    if pos_desired_dict["num"] > pos_current_dict["num"]:
+        # var 1
+        sucess_list.append(chr(pos_current_dict["char"] + 1) +
+                           str(pos_current_dict["num"] + 2))
+
+        # var 2
+        sucess_list.append(chr(pos_current_dict["char"] + 2) +
+                           str(pos_current_dict["num"] + 1))
+
+    else:
+        sucess_list.append(chr(pos_current_dict["char"] + 1) +
+                           str(pos_current_dict["num"] - 2))
+
+        # var 2
+        sucess_list.append(chr(pos_current_dict["char"] + 2) +
+                           str(pos_current_dict["num"] - 1))
+
+else:
+    if pos_desired_dict["num"] > pos_current_dict["num"]:
+        # var 1
+        sucess_list.append(chr(pos_current_dict["char"] - 1) +
+                           str(pos_current_dict["num"] + 2))
+
+        # var 2
+        sucess_list.append(chr(pos_current_dict["char"] - 2) +
+                           str(pos_current_dict["num"] + 1))
+
+    else:
+        sucess_list.append(chr(pos_current_dict["char"] - 1) +
+                           str(pos_current_dict["num"] - 2))
+
+        # var 2
+        sucess_list.append(chr(pos_current_dict["char"] - 2) +
+                           str(pos_current_dict["num"] - 1))
+       
+# answer
+if pos_desired in sucess_list:
+    print("You can make such move")
+else:
+    print("You can not make such move")
+
