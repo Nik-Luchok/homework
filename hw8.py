11a5fdd1517657b7167b73a97bb200d2eabc3e31
Nikita Lukinov <nikita.lukinov.wrk@gmail.com>
2023-02-15 11:49:58 +0100

    started hw8

---
+++ hw8.py
@@ -0,0 +1,9 @@
+"""
+find the biggest powwer of 2, that don't exceed given number
+
+"""
+
+number = input("Positive integer: ")
+
+if not number.isdigit():
+    print("Please give valid positive integer")
