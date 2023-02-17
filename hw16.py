86019002720eb6f8558e74455ac3bd323634517c
Nikita Lukinov <nikita.lukinov.wrk@gmail.com>
2023-02-17 12:49:10 +0100

    hw16, hw17 done tested

--- hw16.py
+++ hw16.py
@@ -6,6 +6,13 @@
 #     * * * * * * * * *
 #   * * * * * * * * * * *
 # * * * * * * * * * * * * *
+#   *                   * 
+#     *               *
+#       *           *
+#         *       *
+#           *   *
+#             *
+
 
 # collecting user input
 rows = input("Height of the pyramid: ")
@@ -20,6 +27,7 @@ except ValueError:
     print("Provide valid integer value")
     exit()
 
+# print the top part
 for row in range(rows):
     # print the first pyramid
     # print white space
@@ -32,6 +40,32 @@ for row in range(rows):
 
     # print the second pyramid, which height is lower by 1
     for cell in range(row):
+        print("*", end=" ")
+
+    # print newline
+    print()
+
+# print the bottom part
+for row in range(1, rows):
+    # print the first pyramid
+    # print white space
+    for cell in range(row):
+        print("  ", end="")
+
+    # print stars
+    print("*", end=" ")
+
+    # print white space
+    for cell in range(rows - (row + 1)):
+        print("  ", end="")
+
+    # print the second pyramid, which height is lower by 1
+    # print white space
+    for cell in range(rows - (row + 1) - 1):
+        print("  ", end="")
+
+    # print star
+    if row != rows - 1:
         print("*", end=" ")
 
     # print newline
