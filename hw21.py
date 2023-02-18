cbaab4931ef371c32fa07b26f8f5bdd172d01d49
Nikita Lukinov <nikita.lukinov.wrk@gmail.com>
2023-02-18 16:03:55 +0100

    hw21 done, tested

---
+++ hw21.py
@@ -0,0 +1,63 @@
+"""
+
+Given multiple strings. Find out how many times
+appears the most used word in the whole text, using dict.
+
+"""
+
+# initialize word string, words_dict - dictionary
+word = ''
+words_dict = {}
+
+# promt user for an input
+text1 = input("Enter text: ")
+text2 = input("Enter text: ")
+text3 = input("Enter text: ")
+
+# concatenate string into one text, add whitespace between
+text = text1 + ' ' + text2 + ' ' + text3
+
+# iterate thru text to find words
+for ch in text:
+    # check a character wether it's alphabetical
+    # or apostrof, but not at the start of the word
+    if ch.isalpha() or (ch == "'" and len(word) != 0):
+        # append the char to a word
+        word += ch
+
+    # if the word endet, and we encountered non alpha symbol
+    elif len(word) != 0:
+        # if the word present in dict
+        if word in words_dict:
+            # add + 1 to the counter
+            words_dict[word] += 1
+
+            # delete the word for the next one
+            word = ''
+
+        # new word, add to the dict
+        else:
+            words_dict[word] = 1
+
+            # delete the word for the next one
+            word = ''
+
+# if text endet on word, without a punctuation symbol
+if len(word):
+    if word in words_dict:
+        # add + 1 to the counter
+        words_dict[word] += 1
+
+    # new word, add to the dict
+    else:
+        words_dict[word] = 1
+
+# find out how many times were used the most used words
+words_max = max(words_dict.values())
+
+# iterate dict, print the words that have this max value
+print("Most used words are:")
+for word in words_dict:
+    if words_dict[word] == words_max:
+        print(word, end=" ")
+print()
